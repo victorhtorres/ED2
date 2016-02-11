53a1175a7f5f1f93f10deba0494e770e44a62d10
Victor H. Torres <vtorresm86@gmail.com>
2016-02-10 23:11:03 -0500

    Añadida función por cola recursiva.

--- Java/ExponencialRecursivo.java
+++ Java/ExponencialRecursivo.java
@@ -15,3 +15,21 @@ public class Exponencial {
     }
   }
 }
+
+// Por cola recursiva
+
+public class ExponencialColaRecursiva {
+
+  public static double ex_v2(double x, int n) {
+    return exExtend(x, 1, n, 1, 1, 0);
+  }
+
+  private static double exExtend(double x, int c, int n, double potx, double factn, double r) {
+    if (n == 0) {
+      return potx / factn + r;
+    } else {
+      return exExtend(x, c + 1, n - 1, (potx * x), (factn * c), r + (potx/factn) );
+    }
+  }
+  
+}
