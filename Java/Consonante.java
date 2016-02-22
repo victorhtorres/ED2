6f6acf8f69d44d496628e5cd110cfc63377c62f7
Víctor H. Torres <vtorresm86@gmail.com>
2016-02-22 11:14:02 -0500

    Se agregó toUpperCase en variable temporal

--- Java/Consonante.java
+++ Java/Consonante.java
@@ -1,21 +1,25 @@
 
 public class Consonantes {
 
-
-// Recursiva de pila
-  
   public static String extraerConsonantes(String cadena) {
-    cadena = cadena.toUpperCase();
+    
     if (cadena.isEmpty()) {
+      
       return "";
+      
     } else {
-      char temp = cadena.charAt(cadena.length()-1);
+      
+      char temp = cadena.toUpperCase().charAt(cadena.length()-1);
       if (temp == 'A' || temp == 'E' || temp == 'I' || temp == 'O' || temp == 'U') {
+        
         return extraerConsonantes(cadena.substring(0, cadena.length()-1));
-      } 
+        
+      }
+      
       return extraerConsonantes(cadena.substring(0, cadena.length()-1)) + cadena.charAt(cadena.length()-1);
+      
     }
+    
   }
  
-  
 }
