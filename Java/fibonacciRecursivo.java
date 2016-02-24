23d5ca89ac6b48dab8edf6a54750a33ac3164a29
Víctor H. Torres <vtorresm86@gmail.com>
2016-02-24 19:24:19 -0300

    Update fibonacciRecursivo.java

--- Java/fibonacciRecursivo.java
+++ Java/fibonacciRecursivo.java
@@ -2,14 +2,25 @@ public class Fibonacci {
 
   public static int fibo(int n) {
 
-    if (n == 0) {     
-      return 0;     
-    } else if (n == 1) {     
-      return 1;   
+    if (n == 0 || n == 1) {     
+      return 1;     
     } else {   
       return fibo((n - 1)) + fibo((n - 2));   
     }
     
+  }
+  
+  //cola [pendiente por terminar....]
+  
+  public static int fiboCola(int n) {
+    return fiboCola(n, 1);
+  }
+  
+  private static int fiboCola(int n, int r) {
+    if (n == 0 || n == 1) {     
+      return r;
+  } else {
+    return fiboCola(n-1, (n-1) + (n-2));
   }
   
 }
